9d6f0e2ef0fb5fa4e66c695600fec484b6664b6f
orinayo <orinayooyelade@gmail.com>
2018-10-26 06:27:29 +0100

    added backend url

--- src/_config/config.js
+++ src/_config/config.js
@@ -3,7 +3,7 @@ const apiVersion = 'v1'
 
 const hostname = window && window.location && window.location.hostname
 
-if (hostname === 'https://afternoon-refuge-91691.herokuapp.com') {
+if (hostname === 'https://lit-basin-96922.herokuapp.com') {
   backendHost = 'https://afternoon-refuge-91691.herokuapp.com'
 } else {
   backendHost = 'http://localhost:3001' || process.env.REACT_APP_BACKEND_HOST
